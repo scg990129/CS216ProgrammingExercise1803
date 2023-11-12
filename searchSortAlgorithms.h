2ca80a210339ef16025b42452ebc198a94a4ac46
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-11-12 14:53:52 -0800

    Initial commit

---
+++ searchSortAlgorithms.h
@@ -0,0 +1,16 @@
+//
+// Created by Jacob Yeung on 11/12/23.
+//
+
+template<class elemType>
+int seqOrdSearch(const elemType list[], int length, const elemType& item)
+{
+    // Write the function definition here
+    return -1; // return -1 if item not found
+} //end seqOrdSearch
+
+
+//#ifndef CS216PROGRAMMINGEXERCISE1803_SEARCHSORTALGORITHMS_H
+//#define CS216PROGRAMMINGEXERCISE1803_SEARCHSORTALGORITHMS_H
+//
+//#endif //CS216PROGRAMMINGEXERCISE1803_SEARCHSORTALGORITHMS_H
