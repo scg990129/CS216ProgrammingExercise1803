cf58e9cd918b633a2e03703dcc8e63fc2668d003
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-11-18 21:34:08 -0800

    completed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "searchSortAlgorithms.h"
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int intList[] = {2, 16, 34, 45, 53,
+                     56, 69, 70, 75, 96};
+
+    int intList2[] = {2, 16, 34, 45, 53,
+                      56, 69, 70, 75, 96, 99, 101,2};
+
+    int pos;
+
+    pos = seqOrdSearch(intList, 10, 45);
+
+    pos = seqOrdSearch(intList, 10, 99);
+
+    pos = seqOrdSearch(intList2, 13, 2);
+
+    pos = seqOrdSearch(intList2, 13, 56);
+
     return 0;
 }
 
--- searchSortAlgorithms.h
+++ searchSortAlgorithms.h
@@ -5,7 +5,12 @@
 template<class elemType>
 int seqOrdSearch(const elemType list[], int length, const elemType& item)
 {
-    // Write the function definition here
+    for(int i = 0; i < length; ++i){ // Write the function definition here
+        if(list[i] == item){
+            return i;
+        }
+    }
+
     return -1; // return -1 if item not found
 } //end seqOrdSearch
 
